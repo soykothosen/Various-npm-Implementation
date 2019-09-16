93966c12567a214f9c9e82c1b3020df8289b15f0
Soykot Hosen <soykothosen1603028@gmail.com>
2019-09-16 06:05:29 +0600

    EJS work successfully

--- app.js
+++ app.js
@@ -4,19 +4,14 @@
  var bodyParser = require('body-parser');
 
  app.use(bodyParser.urlencoded({ extended: true }));
+ app.set('view engine','ejs');
+
+ var myname = "Soykot"
 
  app.get('/',function(req,res){
- 	res.sendFile(path.join( __dirname + '/form.html'));
+ 	res.render('student',{name : myname });
  });
 
-
- app.post('/',function(req,res){
-
- 	var Email = req.body.email;
- 	var Password = req.body.password;
-  res.send("Email Address is" + Email + "& Password is" + Password);
-
- });
 
  app.listen('8080',function(err){
  	if(err){
